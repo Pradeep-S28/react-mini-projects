6be541a3f613de05b001c59cdd31bc1c7d388030
“Pradeep-S28” <emails2pradeep1506@gmail.com>
2026-02-19 15:08:51 +0000

    01_todo_list add task, delete task functionality added

--- 01_todo_list/src/App.jsx
+++ 01_todo_list/src/App.jsx
@@ -4,6 +4,20 @@ import "./App.css";
 function App() {
   const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState("");
+
+  const addData = () => {
+    if (input.trim) {
+      setTasks([...tasks, input]);
+      setInput("");
+    }
+  };
+
+  const removeTask = (index) => {
+    if (confirm("Are you sure to delete ?")) {
+      let filtered_tasks = tasks.filter((task, id) => id != index);
+      setTasks(filtered_tasks);
+    }
+  };
   return (
     <>
       <h1>Todo APP</h1>
@@ -13,7 +27,16 @@ function App() {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <button>Add</button>
+      <button onClick={addData}>Add</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span>{task}</span>
+            <button onClick={() => removeTask(index)}>X</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
