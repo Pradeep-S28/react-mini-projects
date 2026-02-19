7b2f3e9ccf315b8e6aad73645ee0b2144428030d
“Pradeep-S28” <emails2pradeep1506@gmail.com>
2026-02-19 14:57:14 +0000

    01_todo_list - enabled input functionality

--- 01_todo_list/src/App.jsx
+++ 01_todo_list/src/App.jsx
@@ -2,9 +2,18 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
   return (
     <>
       <h1>Todo APP</h1>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button>Add</button>
     </>
   );
 }
